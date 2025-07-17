4a36fa1167cfefeca9658c46d6d05ad0b97c5ffc
Modidev168 <modidev456@gmail.com>
2025-07-17 11:46:59 +0530

    modify main.py and requirements.txt

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-
+# import scikit-learn
 # Load data and model
 data = pd.read_csv('Cleaned_data.csv')
 pipe = pickle.load(open("RidgeModel.pkl", "rb"))
